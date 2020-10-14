e4790443e37e56d91abbb6433a897b15104ece0e
Swagat Ranjit <swagat@zapien.com.au>
2020-10-14 20:12:21 +0545

    Updates

---
+++ precache-manifest.123aa3e92b615e57a0b43af63a27c35c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "462267780429e6cccd68",
+    "url": "/swagat.github.io/static/css/main.24e64811.chunk.css"
+  },
+  {
+    "revision": "462267780429e6cccd68",
+    "url": "/swagat.github.io/static/js/main.7c5005e6.chunk.js"
+  },
+  {
+    "revision": "70ebad03f3989524edb2",
+    "url": "/swagat.github.io/static/js/runtime~main.2f5ca1a6.js"
+  },
+  {
+    "revision": "d7b306731622a042549d",
+    "url": "/swagat.github.io/static/js/2.b56d0cd8.chunk.js"
+  },
+  {
+    "revision": "7e9570315b5ef9753b02eeb5935e892a",
+    "url": "/swagat.github.io/index.html"
+  }
+];
